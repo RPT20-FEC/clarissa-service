e81b73acc076dd132bdee2c1d5e0657c3465f185
clarissajaime <clarissanjaime@gmail.com>
2020-05-15 16:16:38 -0700

    Add arrow functionality to slideshow

--- client/components/ImageView.jsx
+++ client/components/ImageView.jsx
@@ -8,8 +8,6 @@ const ImageView = function () {
   let { id, photoId } = useParams();
   const photos = useFetchPhotos(id);
 
-  const nextImage = function () {};
-
   return (
     <div className="slideshow">
       {photos.map((photo, index) => {
@@ -17,20 +15,32 @@ const ImageView = function () {
           photo._id === photoId && (
             <React.Fragment key={photo._id}>
               <div className="main-image">
-                <Link to={`/${id}/image/${photo._id}`}>
-                  <i className="material-icons">keyboard_arrow_left</i>
-                </Link>
+                {index !== 0 ? (
+                  <Link to={`/${id}/image/${photos[index - 1]._id}`}>
+                    <i className="material-icons">keyboard_arrow_left</i>
+                  </Link>
+                ) : (
+                  <i className="material-icons md-inactive">
+                    keyboard_arrow_left
+                  </i>
+                )}
                 <img key={photo._id} src={photo.url} alt={photo.description} />
-                <Link to={`/${id}/image/${photo._id}`}>
-                  <i className="material-icons">keyboard_arrow_right</i>
-                </Link>
+                {index !== photos.length - 1 ? (
+                  <Link to={`/${id}/image/${photos[index + 1]._id}`}>
+                    <i className="material-icons">keyboard_arrow_right</i>
+                  </Link>
+                ) : (
+                  <i className="material-icons md-inactive">
+                    keyboard_arrow_right
+                  </i>
+                )}
               </div>
               <div className="sidebar">
                 <Link to={`/${id}/`} className="sidebar-link">
                   <i className="material-icons">close</i>
                 </Link>
                 <p className="counter">
-                  {index + 1} / {photos.length + 1}
+                  {index + 1} / {photos.length}
                 </p>
                 <p>{photo.description}</p>
               </div>
