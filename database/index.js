bba04b7dfb9f4fce804ea5a2ee01025008eb8dba
clarissajaime <clarissanjaime@gmail.com>
2020-05-18 18:44:21 -0700

    Fix db warnings

--- database/index.js
+++ database/index.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const mongoUri = "mongodb://localhost:27017/photo-service";
 
-const db = mongoose.connect(mongoUri);
+const db = mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set("useCreateIndex", true);
 
 module.exports = db;
